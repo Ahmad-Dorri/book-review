b0bb44ae10231d0614149a38565ab3670059b958
Ahmad-Dorri <ahmaddorri110@gmail.com>
2024-07-18 12:33:45 +0330

    Create local query scopes for Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,12 +6,37 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title'];
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query
+            ->withCount('reviews')
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query
+            ->withAvg('reviews', 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeLastMonth(Builder $query): Builder
+    {
+        return $query->whereYear('created_at', date('Y'))->whereMonth('created_at', date('m'));
+    }
+
+    public function scopeLastYear(Builder $query): Builder
+    {
+        return $query->whereYear('created_at', date('Y'));
+    }
 
     public function author(): BelongsTo
     {
