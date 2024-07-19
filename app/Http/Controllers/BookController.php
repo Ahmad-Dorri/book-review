9aa4aeaf9f1ab2a2f90b379ea6f943ebbd650eb8
Ahmad-Dorri <ahmaddorri110@gmail.com>
2024-07-19 08:26:28 +0330

    Create a resource controller for book model

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,7 +4,42 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'hello';
+    }
+
+    public function show(string $id)
+    {
+        return 'single book';
+    }
+
+    public function create()
+    {
+        return 'create book form';
+    }
+
+    public function store(Request $request)
+    {
+        return 'store book';
+    }
+
+    public function edit(string $id)
+    {
+        return 'edit book form';
+    }
+
+    public function update(Request $request, string $id)
+    {
+    }
+
+    public function destroy(Request $request, string $id)
+    {
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('index');
+
+Route::resource('books', BookController::class);
