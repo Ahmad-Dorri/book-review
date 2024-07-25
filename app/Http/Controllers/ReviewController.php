8ff2ff288efe1e1c06263d968d2e56fad26ac9ff
Ahmad-Dorri <ahmaddorri110@gmail.com>
2024-07-25 08:43:22 +0330

    Add rate limiter for adding reviews

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -11,6 +11,11 @@ use Illuminate\Http\Request;
 class ReviewController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('throttle:reviews')->only(['store']);
+    }
+
     public function store(Request $request, Book $book)
     {
         $data = $request->validate([
